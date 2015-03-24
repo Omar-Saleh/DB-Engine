54873e80635e896b4516bc0998ba8a3df85b4556
Omar Saleh <omarsaleh0106@gmail.com>
2015-03-24 22:08:35 +0200

    Testing the insert function and adding main method

--- Page/src/Table.java
+++ Page/src/Table.java
@@ -1,8 +1,10 @@
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Hashtable;
@@ -25,26 +27,39 @@ public class table implements Serializable {
 	public void insertIntoPage(Hashtable<String, String> values) throws IOException , ClassNotFoundException {
 		p_index = (size / 200) + 1;
 		try {
-			ObjectInputStream in = new ObjectInputStream(new FileInputStream(new File("page" + p_index)));
+			ObjectInputStream in = new ObjectInputStream(new FileInputStream(new File(name +"page" + p_index)));
 			page load = (page) in.readObject();
 			in.close();
 			Set<String> keys = values.keySet();
 			for(String key : keys) {
 				int insert = cNames.get(key);
-				load.data[load.num][insert] = values.get(key);
-				load.num++;
+				System.out.println(insert + " " + load.index);
+				load.data[load.index][insert - 1] = values.get(key);
 			}
-			
+			load.index = load.index + 1;
+			size++;
+			ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(new File(name + "page" + p_index)));
+			out.writeObject(load);
+			out.close();
 		}
 		catch(FileNotFoundException e) {
 			new page(name ,p_index, cols);
 			insertIntoPage(values);
 		}
+		System.out.println("here");
 	}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws Exception {
+//		table x = new table("test", 2);
+//		x.cNames.put("Name", 1);
+//		x.cNames.put("Age", 2);
+//		Hashtable<String, String> values = new Hashtable<String, String>();
+//		values.put("Name", "Ahmad");
+//		values.put("Age" , "5");
+//		x.insertIntoPage(values);
+		ObjectInputStream in = new ObjectInputStream(new FileInputStream(new File("testpage1")));
+		page test = (page) in.readObject();
+		System.out.println(test.data[0][0] + " " + test.data[0][1]);
 	}
 
 }
